de98648d52c6d786e610c4f8b95fa98791b9ad89
Mikhail Protopopov <mgprotopopov@gmail.com>
2021-03-04 15:24:31 +0700

    Add functions to Weapon Class

--- src/com/company/Weapon.java
+++ src/com/company/Weapon.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class Weapon {
+    public Weapon(int newId) {
+        id = newId;
+        damage = (int)Math.random() * 1000 + 1000;
+        weaponName += (char)('A' + (int)Math.random() * 27);
+        weaponName += (char)('A' + (int)Math.random() * 27);
+        weaponName += '-';
+        weaponName += (int)Math.random() * 899 + 100;
 
+    }
 
+    void showStats() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + weaponName);
+        System.out.println("Damage: " + damage);
+    }
+
+    String weaponName = "";
+    int damage;
+    int id;
 }
