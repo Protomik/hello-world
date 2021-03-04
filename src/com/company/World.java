5aaf76dd3b68d16e09c0e3a63b66a3d8ae48c1f0
Mikhail Protopopov <mgprotopopov@gmail.com>
2021-03-04 15:37:00 +0700

    Add Weapons Class

--- src/com/company/World.java
+++ src/com/company/World.java
@@ -3,51 +3,31 @@ package com.company;
 public class World {
     /* Public methods */
 
-    void setWidth(int newValue) {
-        width = newValue;
+    public World(String name, int newId) {
+        countryName = name;
+        id = newId;
+
     }
 
-    void setHeight(int newValue) {
-        height = newValue;
-    }
+    void setPopulation(int newValue) { population = newValue; }
 
-    void setPopulation(int newValue) {
-        if (height > 0 && width > 0)
-            population = newValue;
-        else
-            System.out.println("There is nothing to populate!");
-    }
-
-    int getWidth() {
-        return width;
-    }
-
-    int getHeight() {
-        return height;
-    }
+    void setId(int newId) { id = newId; }
 
     int getPopulation() {
         return population;
     }
 
-    void showWidth() {
-        System.out.println("Width is: " + width);
-    }
+    int getId() { return id; }
 
-    void showHeight() {
-        System.out.println("Height is: " + height);
-    }
-
-    void showPopulation() {
-        if (population > 0)
-            System.out.println("Hello World! Population is: " + population);
-        else
-            System.out.println("Population is: " + 0);
+    void showStats() {
+        System.out.println("ID: " + id);
+        System.out.println("Stats of " + countryName + ":");
+        System.out.println("Population is " + population + " million" + "\n");
     }
 
     /* Private properties */
 
-    private int width = 0;
-    private int height = 0;
-    private int population = 0;
+    private int population = (int) (Math.random() * 100 + 100);
+    private String countryName = "";
+    private int id = 0;
 }
